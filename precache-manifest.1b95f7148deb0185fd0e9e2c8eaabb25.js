769ac849c9ff7aae6a9e290c3787df6a0d45b88c
Faisal Ahmed <faisal.ahmed.cse.sust@gmail.com>
2019-10-10 12:08:02 +0600

    Updates

---
+++ precache-manifest.1b95f7148deb0185fd0e9e2c8eaabb25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ce3439047bb463dc585c805ed22755",
+    "url": "/kotha-bolun/index.html"
+  },
+  {
+    "revision": "f0144b08b04aacad12fa",
+    "url": "/kotha-bolun/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7be9b60bd3b03d2eb119",
+    "url": "/kotha-bolun/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f0144b08b04aacad12fa",
+    "url": "/kotha-bolun/static/js/2.1432759e.chunk.js"
+  },
+  {
+    "revision": "7be9b60bd3b03d2eb119",
+    "url": "/kotha-bolun/static/js/main.2bf157fd.chunk.js"
+  },
+  {
+    "revision": "0116aa26c3e474212e87",
+    "url": "/kotha-bolun/static/js/runtime-main.211cf714.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kotha-bolun/static/media/logo.25bf045c.svg"
+  }
+]);
