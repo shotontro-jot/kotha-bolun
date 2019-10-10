e96b2d50980fb3e4ac41ae5a3626e98ed96078a9
Faisal Ahmed <faisal.ahmed.cse.sust@gmail.com>
2019-10-10 11:17:57 +0600

    42

---
+++ build/precache-manifest.0c2341406ba52c0c132d3a036cbe9d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d613a7bfbf5d22f182e8335547a2914",
+    "url": "/shotontrojot-kotha-bolun/index.html"
+  },
+  {
+    "revision": "f0144b08b04aacad12fa",
+    "url": "/shotontrojot-kotha-bolun/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5fca6d62acf5e3ca1c3a",
+    "url": "/shotontrojot-kotha-bolun/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f0144b08b04aacad12fa",
+    "url": "/shotontrojot-kotha-bolun/static/js/2.1432759e.chunk.js"
+  },
+  {
+    "revision": "5fca6d62acf5e3ca1c3a",
+    "url": "/shotontrojot-kotha-bolun/static/js/main.34cd28df.chunk.js"
+  },
+  {
+    "revision": "8323e0e4bb13fe3376ec",
+    "url": "/shotontrojot-kotha-bolun/static/js/runtime-main.29fbdd03.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shotontrojot-kotha-bolun/static/media/logo.25bf045c.svg"
+  }
+]);
