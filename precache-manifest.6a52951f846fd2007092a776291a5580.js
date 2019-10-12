4f858e3e75dacd503fc5a529d46c46ec7d5c4c6d
Faisal Ahmed <faisal.ahmed.cse.sust@gmail.com>
2019-10-12 23:11:47 +0600

    Updates

--- precache-manifest.6a52951f846fd2007092a776291a5580.js
+++ precache-manifest.6a52951f846fd2007092a776291a5580.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d949a4577ec8297bfb8ac9e68c257fd8",
+    "revision": "0463d94401ed7865386ebbd15d902bf6",
     "url": "/kotha-bolun/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kotha-bolun/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "f6a8d1685fb5331ba60b",
+    "revision": "4c5c081774ced116fa3b",
     "url": "/kotha-bolun/static/css/main.e197682c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/kotha-bolun/static/js/2.97ab21d0.chunk.js"
   },
   {
-    "revision": "f6a8d1685fb5331ba60b",
-    "url": "/kotha-bolun/static/js/main.06967ef5.chunk.js"
+    "revision": "4c5c081774ced116fa3b",
+    "url": "/kotha-bolun/static/js/main.c8fe26b3.chunk.js"
   },
   {
     "revision": "0116aa26c3e474212e87",
